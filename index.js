9df73ff0fd529abbfebd52b6d3f4f73a72ca49b0
Daniel Vu <daniel.vu@cerner.net>
2018-03-27 20:08:19 -0500

    Add MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+
 const PORT = process.env.PORT || 5000;
+const DB_USER = process.env.BUDGET_BUILDER_DB_USER;
+const DB_PASSWORD = process.env.BUDGET_BUILDER_DB_PASSWORD;
+
+if (!DB_USER || !DB_PASSWORD) {
+  throw new Error('Database username or password environment variables are not defined');
+}
+
+const connectionString = `mongodb://${DB_USER}:${DB_PASSWORD}@ds125469.mlab.com:25469/budget-builder`;
+MongoClient.connect(connectionString, (err, client) => {
+  if (err) {
+    console.log('Error connecting to database: ', err);
+  }
+
+  console.log('Connected to database');
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
