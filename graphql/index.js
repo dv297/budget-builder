66716a1372b1b261908ba90863de1d680f3ad58a
Daniel Vu <daniel.vu@cerner.net>
2018-03-31 15:08:30 -0500

    Update to use GraphQLSchema

--- graphql/index.js
+++ graphql/index.js
@@ -1,21 +1,58 @@
-const { buildSchema } = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList } = require('graphql');
 const { getDatabase } = require('../database');
+const { ObjectId } = require('mongodb');
 
-const schema = buildSchema(`
-  type Query {
-    user(id: Int!): User
-  }
-  type User {
-    username: String,
-  }
-`);
-
-const root = {
-  user: async () => {
-    const users = getDatabase().collection('users');
-    return users.findOne({});
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    _id: { type: GraphQLString },
+    username: { type: GraphQLString },
   },
-};
+});
+
+const schema = new GraphQLSchema({
+  mutation: new GraphQLObjectType({
+    name: 'mutations',
+    fields: {
+      user: {
+        type: UserType,
+        args: {
+          username: { type: GraphQLString },
+        },
+        async resolve(rootValue, { username }) {
+          const users = getDatabase().collection('users');
+          const insertProcess = await users.insertOne({ username });
+
+          if (insertProcess.result.n === 1 && insertProcess.result.ok === 1) {
+            return users.findOne({ _id: insertProcess.insertedId });
+          }
+        }
+      }
+    }
+  }),
+  query: new GraphQLObjectType({
+    name: 'queries',
+    fields: {
+      users: {
+        type: GraphQLList(UserType),
+        async resolve() {
+          const users = await getDatabase().collection('users').find().toArray();
+          return users;
+        }
+      },
+      user: {
+        type: UserType,
+        args: {
+          id: { type: GraphQLString },
+        },
+        async resolve(parent, { id }) {
+          const users = getDatabase().collection('users');
+          return users.findOne({ _id: ObjectId(id) });
+        },
+      },
+    },
+  }),
+});
 
 module.exports = {
   schema,
